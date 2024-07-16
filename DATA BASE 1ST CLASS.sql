c084744a88e0f3e5abc72e4549c9e01ad6e8132c
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-07-16 07:56:56 +0530

    first commit

---
+++ DATA BASE 1ST CLASS.sql
@@ -0,0 +1,25 @@
+-- To create database
+CREATE DATABASE mohammad_rayyan; 
+
+-- To see all databases
+SHOW DATABASES;
+
+-- To select database (syntex_USE db_name)
+USE petshop;
+-- To show all tables first used database and then (show table)
+SHOW TABLES;
+
+USE mohammad_rayyan;
+
+SHOW TABLES;
+
+-- To delete database (DROP DATABASE db_name)
+DROP DATABASE mohammad_rayyan;
+SHOW DATABASES;
+--  create table
+CREATE TABLE employee(
+employee_id INT,
+first_name VARCHAR(50),
+Last_name VARCHAR(50)
+);
+SHOW TABLES;
