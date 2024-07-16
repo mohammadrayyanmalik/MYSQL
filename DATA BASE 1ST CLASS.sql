a9b6ba642a4f8589a06d1bcbebecd72222e182dc
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-07-16 08:58:49 +0530

    second

--- DATA BASE 1ST CLASS.sql
+++ DATA BASE 1ST CLASS.sql
@@ -23,3 +23,40 @@ first_name VARCHAR(50),
 Last_name VARCHAR(50)
 );
 SHOW TABLES;
+
+
+CREATE TABLE student(
+rool_no INT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+age INT,
+birth_date DATE,
+phone_no INT
+);
+
+-- desc (describe tables detials)
+DESC employee;
+
+
+-- ALTER used for edit in existing table either add or delete
+-- ALTER TABLE < table_name> ADD COLUMN <column_name> <data_type>
+ALTER TABLE student ADD COLUMN address VARCHAR(50);
+DESC student;	
+
+
+-- ALTER TABLE < table_name> ADD COLUMN <new_column_name> <data_type> AFTER < EXISTING_COLUMN_NAME>
+ALTER TABLE student ADD COLUMN teacher_name VARCHAR(50) AFTER age;
+DESC student;
+
+-- ALTER TABLE <table_name> ADD COLUMN< new_column_name> <data_type> FIRST;
+ALTER TABLE student ADD COLUMN email VARCHAR (80) FIRST;
+DESC student;
+
+
+--  to delete existing column
+-- ALTER TABLE <table_name> DROP COLUMN <COLUMN NAME>;
+ALTER TABLE student DROP COLUMN email;
+
+
+DROP TABLE employee;
+SHOW TABLES;
